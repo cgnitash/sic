716102c4b803ec2b8914e34af93c74645501fa65
cgnitash <cgnitash@gmail.com>
2022-05-22 00:08:55 -0400

    moved files to src directory

---
+++ src/ensemble.hpp
@@ -0,0 +1,69 @@
+
+#include <iostream>
+#include <map>
+#include <set>
+#include <string>
+#include <utility>
+#include <vector>
+
+struct EnsembleError{};
+
+class Sequence
+{
+public:
+  std::string sequence;
+  double      weight = 0.;
+  std::string id;
+};
+
+class Ensemble
+{
+public:
+  void load(std::string const &);
+  void load_tests(std::string const &fn);
+
+  void
+      enable_weights()
+  {
+    use_weights = true;
+  }
+
+  void summary();
+
+  void generate_pwms_and_true_scores(int);
+
+
+private:
+  std::string                         ensemble_file_name;
+  std::vector<Sequence>               sequences;
+  std::map<int, int>                  length_counts;
+  std::map<char, std::pair<int, int>> symbol_counts;
+  std::vector<std::vector<double>>    pwm_1;
+  std::vector<std::vector<double>>    pwm_2;
+  std::vector<std::vector<double>>    pwm_3;
+  std::vector<char>                   symbols;
+  int pwm_order = 0;
+  // std::vector<double>                 true_scores_1;
+  // std::vector<double>                 true_scores_2;
+  // std::vector<double>                 true_scores_3;
+  double total_weight = 0;
+  bool   use_weights  = false;
+
+  void   generate_pwm_1();
+  void   calculate_true_scores_1();
+  double calculate_individual_score_1(Sequence const &);
+
+  void   generate_pwm_2();
+  void   calculate_true_scores_2();
+  double calculate_individual_score_2(Sequence const &);
+
+  void   generate_pwm_3();
+  void   calculate_true_scores_3();
+  double calculate_individual_score_3(Sequence const &);
+
+  int symbol_index(char) const;
+  int C(int, int);
+
+  bool check_validity(Sequence const &);
+};
+
---
+++ src/main.cpp
@@ -0,0 +1,63 @@
+
+#include <fstream>
+
+#include "clap.hpp"
+#include "ensemble.hpp"
+
+int
+    main(int argc, char **argv)
+try
+{
+  CommandLineArgParser c;
+  c.add_argument("Ensemble",
+                 "Ensemble file used to generate PWM",
+                 { "-e", "--ensemble" },
+                 {},
+                 "");
+  c.add_argument("Test",
+                 "Test file containing sequences to score based on PWM",
+                 { "-t", "--test" },
+                 {},
+                 "");
+  c.add_argument("UseWeights",
+                 "Should sequences be weighted? (Y/N)",
+                 { "-w", "--use-weights" },
+                 { "Y", "yes", "N", "no" },
+                 "N");
+  c.add_argument("Summarize",
+                 "Display summary of ensemble data (Y/N)",
+                 { "-s", "--summarize" },
+                 { "Y", "yes", "N", "no" },
+                 "N");
+  c.add_argument("PWMSize",
+                 "Correlation order for PWM",
+                 { "-o", "--order" },
+                 { "1", "2", "3" },
+                 "1");
+  auto const args = c.parse_arguments(argc, argv);
+
+  Ensemble e;
+  e.load(args.at("Ensemble"));
+
+  if (auto const summary = args.at("UseWeights");
+      summary == "Y" or summary == "yes")
+    e.enable_weights();
+
+  if (auto const summary = args.at("Summarize");
+      summary == "Y" or summary == "yes")
+    e.summary();
+
+  e.generate_pwms_and_true_scores(std::stoi(args.at("PWMSize")));
+
+  e.load_tests(args.at("Test"));
+}
+catch (RuntimeError const &)
+{
+}
+catch (EnsembleError const &)
+{
+}
+catch (...)
+{
+  std::cout << "Internal bug: Unknown exception\n";
+}
